1e8c6ce4cef216e3822aa58c3141ba087ea969b2
Ethan Furman <ethan@stoneleaf.us>
2019-02-21 18:47:08 -0800

    include type

--- sample_request.py
+++ sample_request.py
@@ -9,7 +9,7 @@ class sample_request(osv.Model):
     _inherit = 'sample.request'
 
     _columns= {
-        'ship_to_parent_id': fields.related('ship_to_id', 'ship_to_parent_id'),
+        'ship_to_parent_id': fields.related('ship_to_id', 'ship_to_parent_id', type='many2one'),
         }
 
 
