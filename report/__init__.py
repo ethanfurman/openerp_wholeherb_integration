406ead54b019e692a6123ff6696fe142d30602af
Ethan Furman <ethan@stoneleaf.us>
2022-07-21 15:39:31 -0700

    add Job Time report

--- report/__init__.py
+++ report/__init__.py
@@ -4,7 +4,7 @@ from xaml import Xaml as _Xaml
 # report_name = (_Path(__file__).dirname or _Path('.'))/'product-traffic.xaml'
 base_dir = (_Path(__file__).dirname or _Path('.'))
 
-for report in ('product-traffic.xaml', 'inhouse-costing.xaml', 'inhouse-work_order.xaml'):
+for report in ('product-traffic.xaml', 'inhouse-costing.xaml', 'inhouse-job_time.xaml', 'inhouse-work_order.xaml'):
     report_name = base_dir / report
     if not report_name.exists():
         print('skipping', report_name)
